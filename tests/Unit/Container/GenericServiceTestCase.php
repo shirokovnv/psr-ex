301e010873ee8109309fc01bcf0712dba33d537d
shirokovnv <shirokovnv@gmail.com>
2023-09-11 12:26:50 +0500

    Add closure binding test

--- tests/Unit/Container/GenericServiceTestCase.php
+++ tests/Unit/Container/GenericServiceTestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Shirokovnv\PsrEx\Tests\Unit\Container;
 
+use Shirokovnv\PsrEx\Container\Container;
 use Shirokovnv\PsrEx\Container\Exceptions\NotFoundException;
 use Shirokovnv\PsrEx\Tests\Stubs\Services\Service;
 use Shirokovnv\PsrEx\Tests\Stubs\Services\ServiceInterface;
@@ -16,6 +17,18 @@ class GenericServiceTestCase extends ContainerTestCase
     public function testInterfaceBinding(): void
     {
         $this->container->set(ServiceInterface::class, Service::class);
+
+        $instance = $this->container->get(ServiceInterface::class);
+        $this->assertInstanceOf(Service::class, $instance);
+    }
+
+    /**
+     * @return void
+     */
+    public function testClosureBinding(): void
+    {
+        $this->container->set(Service::class, Service::class);
+        $this->container->set(ServiceInterface::class, fn (Container $container) => $container->get(Service::class));
 
         $instance = $this->container->get(ServiceInterface::class);
         $this->assertInstanceOf(Service::class, $instance);
